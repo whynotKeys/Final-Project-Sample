74a73ea743ac82400c145aee881a027cdc0378fc
whynotKeys <wildcat0426@naver.com>
2025-07-07 10:28:50 +0900

    deploy test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 
 export default function Home() {
   return (
-    <h1>This is Final-Project-Sample ଘ(∩◉ω◉ )⊃----♥*♡+:｡.｡ ⍤⃝｡.｡:+♡*♥҉*･｡ﾟ ҉*:.｡</h1>
+    <h1>This is my Final-Project-Sample ଘ(∩◉ω◉ )⊃----♥*♡+:｡.｡ ⍤⃝｡.｡:+♡*♥҉*･｡ﾟ ҉*:.｡</h1>
   );
 }
